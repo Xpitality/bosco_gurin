abdcdc0aeb9127f5cb4b47e555eb6df074d12958
Marko Bošković <bosskovic@gmail.com>
2020-11-27 15:22:52 +0100

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  root to: 'admin/locations#index'
+
   namespace :admin do
-      resources :users
       resources :locations
       resources :events
-      root to: "users#index"
+      resources :users
+      root to: "locations#index"
     end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
