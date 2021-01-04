0d6119ad397740a364b5e2405bb98a96568979fa
Alessio Re <alessio@xpitality.com>
2021-01-04 17:06:22 +0100

    changed config file path

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 export WORKDIR
 
-envsubst '${WORKDIR}' < /tmp/application.conf > /etc/nginx/conf.d/default.conf &&
-envsubst '${WORKDIR}' < /tmp/nginx.conf > /etc/nginx/nginx.conf
+envsubst '${WORKDIR}' < /tmp/application.conf > /opt/bitnami/nginx/conf/server_blocks/default.conf &&
+#envsubst '${WORKDIR}' < /tmp/nginx.conf > /etc/nginx/nginx.conf
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
