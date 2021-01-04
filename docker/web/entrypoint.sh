dc7882b377707261176ea68155322996ec510383
Alessio Re <alessio@xpitality.com>
2021-01-04 17:07:22 +0100

    Uses bitnami nginx path

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 export WORKDIR
 
 envsubst '${WORKDIR}' < /tmp/application.conf > /opt/bitnami/nginx/conf/server_blocks/default.conf &&
-#envsubst '${WORKDIR}' < /tmp/nginx.conf > /etc/nginx/nginx.conf
+envsubst '${WORKDIR}' < /tmp/nginx.conf > /opt/bitnami/nginx/conf/nginx.conf
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
