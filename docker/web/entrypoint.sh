59f3bc291c37197ea9d79ff51fbd25e0ed7cfa21
Alessio Re <alessio@xpitality.com>
2021-01-05 17:16:42 +0100

    debug in entrypoint, server_name in application.conf

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -6,7 +6,10 @@ export WORKDIR
 envsubst '${WORKDIR}' < /tmp/application.conf > /opt/bitnami/nginx/conf/server_blocks/default.conf &&
 envsubst '${WORKDIR}' < /tmp/nginx.conf > /opt/bitnami/nginx/conf/nginx.conf
 
-cat /opt/bitnami/nginx/conf/nginx.conf
+ls /opt/bitnami/nginx
+ls /opt/bitnami/nginx/conf
+ls /opt/bitnami/nginx/conf/server_blocks
+cat /opt/bitnami/nginx/conf/server_blocks/default.conf
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
