c518d2582c483a5edc172bb7a362a2b19db2c2d4
Alessio Re <alessio@xpitality.com>
2021-01-05 17:49:24 +0100

    entrypoint

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
+echo "NGINX"
 ls /opt/bitnami/nginx
+echo "NGINX CONF"
 ls /opt/bitnami/nginx/conf
+echo "SERVER BLOCKS"
 ls /opt/bitnami/nginx/conf/server_blocks
+echo "APP"
 cat /opt/bitnami/nginx/conf/server_blocks/default.conf
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
