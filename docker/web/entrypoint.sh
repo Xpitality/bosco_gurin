c227fa1859cfbec523feb9ccb0b08b071251b39a
Alessio Re <alessio@xpitality.com>
2021-01-05 16:54:30 +0100

    nginx paths

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -6,5 +6,7 @@ export WORKDIR
 envsubst '${WORKDIR}' < /tmp/application.conf > /opt/bitnami/nginx/conf/server_blocks/default.conf &&
 envsubst '${WORKDIR}' < /tmp/nginx.conf > /opt/bitnami/nginx/conf/nginx.conf
 
+cat /opt/bitnami/nginx/conf/nginx.conf
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
