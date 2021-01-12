06aa2293163bc2041aed4da0ef85ac00b38d3e1b
Marko Bošković <bosskovic@gmail.com>
2021-01-12 16:56:10 +0100

    trims down the info snow response to only snow height

--- app/controllers/api/v1/info_snow_controller.rb
+++ app/controllers/api/v1/info_snow_controller.rb
@@ -5,7 +5,21 @@ module Api
     class InfoSnowController < Api::V1::ApplicationController
       def feed
         info_snow_feed = InfoSnowXmlParser.new
-        render json: info_snow_feed.response
+        if params['only-snow-height']
+          resort = info_snow_feed.response[:resort]
+          render json: { resort: {
+              WTDailyNewFallenSnowHeightArenaLow: resort['WTDailyNewFallenSnowHeightArenaLow'],
+              WTDailyNewFallenSnowHeightArenaHigh: resort['WTDailyNewFallenSnowHeightArenaHigh'],
+              WTDailyDateLastSnowfallResort: resort['WTDailyDateLastSnowfallResort'],
+              WTDailyDateLastSnowfallArena: resort['WTDailyDateLastSnowfallArena'],
+              WTDailyDepthSnowOrtResort: resort['WTDailyDepthSnowOrtResort'],
+              WTDailyDepthSnowOrtArenaLow: resort['WTDailyDepthSnowOrtArenaLow'],
+              WTDailyDepthSnowOrtArenaHigh: resort['WTDailyDepthSnowOrtArenaHigh']
+          }
+          }
+        else
+          render json: info_snow_feed.response
+        end
       end
     end
   end
