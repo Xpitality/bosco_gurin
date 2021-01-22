b7c8cfa41678414f2b197e64f8d15d71d6a9161c
Marko Bošković <bosskovic@gmail.com>
2021-01-22 18:06:42 +0100

    asks for confirmation when pushing, allows visibility toggling

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -3,6 +3,23 @@ require 'fcm_sender'
 module Admin
   class NotificationsController < Admin::ApplicationController
 
+    def update
+      if resource_params["visible_in_app"] == "1"
+        Notification.update_all(visible_in_app: false)
+      end
+
+      if requested_resource.update(resource_params)
+        redirect_to(
+            [namespace, requested_resource],
+            notice: translate_with_resource("update.success"),
+            )
+      else
+        render :edit, locals: {
+            page: Administrate::Page::Form.new(dashboard, requested_resource),
+        }
+      end
+    end
+
     def push
       notification = Notification.find params[:notification_id]
 
