bf4e0fad31936fc17b92c2af6742502ffc11caca
Marko Bošković <bosskovic@gmail.com>
2021-04-01 16:18:51 +0200

    adds locations seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,16 @@ user = User.find_or_create_by!(email: ENV['ADMIN_EMAIL']) do |user|
   user.password_confirmation = ENV['ADMIN_PASSWORD']
   # user.confirm
 end
+
+
+puts '---', ' ', 'Seeding locations', ' '
+[
+    { name: 'Sonnenberg', weather_forecast: true, lng: 8.447897368060037, lat: 46.313237079597194, elevation: 2400, webcam: 'https://tuks.ch/webcam/bosco7000M.jpg'},
+    { name: 'Guriner Furka', weather_forecast: false, lng: 8.453941633227059, lat: 46.32362148936983, elevation: 2320, webcam: 'https://tuks.ch/webcam/bosco6000M.jpg'},
+    { name: 'Rossboda', weather_forecast: false, lng: 8.46577957067739, lat: 46.315896652863024, elevation: 2000, webcam: 'https://tuks.ch/webcam/bosco1.jpg?1416844635639'},
+    { name: 'Kids Village', weather_forecast: false, lng: 8.487836065277635, lat: 46.315534542944825, elevation: 1500, webcam: 'https://tuks.ch/webcam/bosco3.jpg?1416844686748'},
+    { name: 'Paese', weather_forecast: false, lng: 8.49345797523924, lat: 46.31716477600904, elevation: 1500, webcam: 'https://www.bosco-gurin.ch/bg_webcam.jpg'}
+].each do |location_params|
+  l = Location.create location_params
+  puts "location created: #{l.name}"
+end
